1cff542681a1861216ff017ccf32650f1af4c1c2
Watchanon Numnam <jiramot@gmail.com>
2019-02-19 12:28:26 +0700

    Add gateway

---
+++ gateway/src/main/kotlin/com/jrm/springcloud/gateway/GatewayApplication.kt
@@ -0,0 +1,12 @@
+package com.jrm.springcloud.gateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+
+@SpringBootApplication
+class GatewayApplication
+
+fun main(args: Array<String>) {
+  runApplication<GatewayApplication>(*args)
+}
---
+++ gateway/src/main/kotlin/com/jrm/springcloud/gateway/GatewayDiscoveryConfiguration.kt
@@ -0,0 +1,17 @@
+package com.jrm.springcloud.gateway
+
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator
+import org.springframework.cloud.client.discovery.DiscoveryClient
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableDiscoveryClient
+class GatewayDiscoveryConfiguration {
+  @Bean
+  fun discoveryClientRouteLocator(discoveryClient: DiscoveryClient, properties: DiscoveryLocatorProperties): DiscoveryClientRouteDefinitionLocator {
+    return DiscoveryClientRouteDefinitionLocator(discoveryClient, properties)
+  }
+}
